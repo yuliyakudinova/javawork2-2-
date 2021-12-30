8ca41fd4fd66bde0d3d5c44a584e0c602cb5ba17
yulechka.kudinova@mail.ru <yulechka.kudinova.1991@mail.ru>
2021-12-30 22:09:37 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,15 @@
-
 public class Main {
     public static void main(String[] args) {
-
         int initialAccount = 100;
-        int accountReplenishment = 700;
-        int bonus= accountReplenishment / 100;
-
-        if (accountReplenishment >= 1000){
-            bonus = accountReplenishment /100;
-        }
-        else {
+        int accountReplenishment = 1100;
+        int bonus = accountReplenishment / 100;
+        if (accountReplenishment >= 1000) {
+            bonus = accountReplenishment / 100;
+        } else {
             bonus = 0;
         }
         int totalAmount = initialAccount + accountReplenishment + bonus;
         System.out.println("Общий счет: " + totalAmount);
-        System.out.println( "Ваши бонусы: " + bonus);
-
-
+        System.out.println("Ваши бонусы: " + bonus);
     }
 }
